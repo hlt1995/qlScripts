4d57f122124a7b35defa5a4672ee2e9b2763ff1d
hlt1995 <93117676+hlt1995@users.noreply.github.com>
2025-09-06 22:34:05 +0800

    Update TongchengTravel.py

--- TongchengTravel.py
+++ TongchengTravel.py
@@ -242,4 +242,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-    #send_notification_message_collection(f"同程旅行签到通知 - {datetime.now().strftime('%Y/%m/%d')}")
+    send_notification_message_collection(f"同程旅行签到通知 - {datetime.now().strftime('%Y/%m/%d')}")
