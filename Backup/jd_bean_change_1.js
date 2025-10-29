298abe88159e6890193eb88a399753dfbb2895e8
hlt1995 <93117676+hlt1995@users.noreply.github.com>
2025-10-29 22:59:16 +0800

    Update jd_bean_change_1.js

--- Backup/jd_bean_change_1.js
+++ Backup/jd_bean_change_1.js
@@ -857,10 +857,10 @@ async function showMsg() {
 
     }
 
-    if(strGuoqi){		
-		ReturnMessage += `💸💸💸临期京豆明细💸💸💸\n`;
-		ReturnMessage += `${strGuoqi}`;
-	}
+    // if(strGuoqi){		
+		// ReturnMessage += `💸💸💸临期京豆明细💸💸💸\n`;
+		// ReturnMessage += `${strGuoqi}`;
+	// }
 
     ReturnMessage += `${$.message}`;
     strsummary += `红包${$.balance}元`
