93e0aef79fb30ff43f2d74fb8a0ac891d5dd30c5
hlt1995 <93117676+hlt1995@users.noreply.github.com>
2025-11-20 22:12:10 +0800

    Update Nestle.js

--- Nestle.js
+++ Nestle.js
@@ -1,6 +1,6 @@
 // 大大鸣版 雀巢
 // 环境变量 NESTLE_TOKEN  抓取 Authorization 的值，例如：bearer 0610099d-550e-4e8d-9624-6840ff680812，只需要 bearer 后面的值
-//name: 雀巢
+//name: 雀巢会员
 //cron: 30 12 * * *
  * 变量：export NESTLE_TOKEN="Authorization"
  * 入口：#小程序://雀巢会员/O0NOfAHwAGV3tZb
